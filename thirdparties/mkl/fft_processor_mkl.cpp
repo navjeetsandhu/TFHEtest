469e69df49e03d617ee58eb5d5c5c9df1aaa47d2
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-10-19 11:36:53 -0400

    changes

--- thirdparties/mkl/fft_processor_mkl.cpp
+++
@@ -1,5 +0,0 @@
-#include "fft_processor_mkl.hpp"
-
-// FFT_Processor_MKL is thread-safe
-thread_local FFT_Processor_MKL<TFHEpp::lvl1param::n> fftplvl1;
-thread_local FFT_Processor_MKL<TFHEpp::lvl2param::n> fftplvl2;
