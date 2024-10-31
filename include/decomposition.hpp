fd565ff1e76cdb19ed1e4919204edeb07f7e7685
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-10-31 15:39:00 -0400

    decomposition made easier

--- include/decomposition.hpp
+++ include/decomposition.hpp
@@ -10,6 +10,7 @@
 
 /*
  * https://www.zama.ai/post/tfhe-deep-dive-part-3
+ * Decompose large coefficient into small base (Bg)
  */
 
 namespace TFHEpp {
@@ -34,41 +35,31 @@ inline void Decomposition(DecomposedPolynomial<P> &decpoly,
                           const Polynomial<P> &poly, typename P::T randbits = 0)
 {
 
-    constexpr typename P::T offset = offsetgen<P>(); // offset: lvl2 0x8040201000000000 lvl1 0x82080000
-                                                     // lvl1   l = 3  Bgbit 6, Bg 64
-                                                     // lvl2   l = 4  Bgbit 9, Bg 512
-
-    constexpr uint32_t   roundoffsetBit = std::numeric_limits<typename P::T>::digits - P::l * P::Bgbit - 1;
-    std::cout << std::dec << "roundoffsetBit: " << roundoffsetBit << std::endl;
+    // lvl1 offset 0x82080000,         roundoffsetBit: 13, roundoffset: 0x2000,    totaloffset 0x82082000
+    // lvl2 offset 0x8040201000000000, roundoffsetBit: 27, roundoffset: 0x8000000, totaloffset 0x8040201008000000
+    constexpr typename P::T offset = offsetgen<P>();
+    constexpr uint32_t roundoffsetBit = std::numeric_limits<typename P::T>::digits - P::l * P::Bgbit - 1;
     constexpr typename P::T roundoffset = 1ULL << roundoffsetBit;
-    std::cout << std::hex << "roundoffset: " << roundoffset << std::endl;
+    constexpr typename P::T totaloffset = offset + roundoffset;
 
-
-    constexpr typename P::T mask =
-        static_cast<typename P::T>((1ULL << P::Bgbit) - 1);
-    std::cout << std::hex << "mask: " << mask << std::endl;
-
-
+    constexpr auto mask = static_cast<typename P::T>((1ULL << P::Bgbit) - 1);
     constexpr typename P::T halfBg = (1ULL << (P::Bgbit - 1));
-    std::cout << std::hex << "halfBg: " << halfBg << std::endl;
+    constexpr uint32_t maxDigits = std::numeric_limits<typename P::T>::digits;
 
+    // lvl1   maxDigits 32, l 3  Bgbit 6, Bg 64,   halfBg: 32,    mask: 0x3f (6 digits)
+    // lvl2   maxDigits 64, l 4  Bgbit 9, Bg 512,  halfBg: 256,   mask: 0x1ff (9 digits)
 
     for (int i = 0; i < P::n; i++) {
-        for (int ii = 0; ii < P::l; ii++)
-            decpoly[ii][i] = (((poly[i] + offset + roundoffset) >>
-                              (std::numeric_limits<typename P::T>::digits -
-                               (ii + 1) * P::Bgbit)) & mask) - halfBg;
+        auto valuePlusOffset = poly[i] + totaloffset;
+        for (int ii = 0; ii < P::l; ii++) {
+            auto digitsToShift = maxDigits - (ii + 1) * P::Bgbit;
+            auto shiftedValue = valuePlusOffset >> digitsToShift;
+            auto maskedValue = shiftedValue & mask;
+            decpoly[ii][i] = maskedValue - halfBg;
+        }
     }
 }
 
-template <class P>
-void DecompositionNTT(DecomposedPolynomialNTT<P> &decpolyntt,
-                      const Polynomial<P> &poly, typename P::T randbits = 0)
-{
-    DecomposedPolynomial<P> decpoly;
-    Decomposition<P>(decpoly, poly);
-    for (int i = 0; i < P::l; i++) TwistINTT<P>(decpolyntt[i], decpoly[i]);
-}
 
 template <class P>
 void DecompositionRAINTT(DecomposedPolynomialRAINTT<P> &decpolyntt,
