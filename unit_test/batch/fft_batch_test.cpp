c27868e8d35ba4e3548e7a54f6172c74884eaf8e
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-12-10 16:19:03 -0500

    changes

--- unit_test/batch/fft_batch_test.cpp
+++ unit_test/batch/fft_batch_test.cpp
@@ -6,10 +6,11 @@
 #include <chrono>
 
 template <int nbits,  int batch>
-void test_fft()
+void test_fft(uint32_t num_test)
 {
     constexpr int N = 1 << nbits;
     constexpr unsigned size = batch *N;
+    int i;
     std::array<uint32_t, size> result{};
     std::fill(result.begin(), result.end(), 0);
 
@@ -19,11 +20,11 @@ void test_fft()
     auto start = std::chrono::high_resolution_clock::now();
 
     alignas(64) std::array<double, size> fft{};
-    TwistFpgaIFFTbatch(fft.data(), p1.data(), batch);
+    for (i=0; i< num_test; i++) TwistFpgaIFFTbatch(fft.data(), p1.data(), batch);
 
     auto finish1 = std::chrono::high_resolution_clock::now();
 
-    TwistFpgaFFTbatch(result.data(), fft.data(), batch);
+    for (i=0; i< num_test; i++) TwistFpgaFFTbatch(result.data(), fft.data(), batch);
 
     auto finish = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> elapsed = finish - start;
@@ -31,6 +32,8 @@ void test_fft()
     std::chrono::duration<double, std::milli> elapsed2 = finish - finish1;
 
     std::cout << "batch: " << batch << " s\n";
+    std::cout << "num iterations: " << num_test << std::endl;
+    std::cout << "total run: " << batch * num_test << std::endl;
     std::cout << "Total Elapsed IFFT time: " << elapsed1.count() << " ms\n";
     std::cout << "Total Elapsed FFT time: " << elapsed2.count() << " ms\n";
     std::cout << "Total Elapsed time: " << elapsed.count() << " ms\n";
@@ -40,10 +43,21 @@ void test_fft()
 
 
 
-int main()
+int main(int argc, char* argv[])
 {
+    uint32_t num_test = 10;
+    for(int i = 0; i < argc; i++) {
+        std::cout << "Argument " << i << " is " << argv[i] << std::endl;
+    }
+
+    if(argc > 1) {
+        std::stringstream str_stream(argv[1]);
+        str_stream >> num_test;
+    }
+
+    std::cout << "num iterations: " << num_test << std::endl;
     constexpr int nbit = 10;
-    constexpr int batch = 2500;
-    test_fft<nbit, batch>();
+    constexpr int batch = 20;
+    test_fft<nbit, batch>(num_test);
     return 0;
 }
