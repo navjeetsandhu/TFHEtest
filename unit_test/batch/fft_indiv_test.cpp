73375b122a36fc70b0f520eb06750b0fa3ccf8ab
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-12-10 14:46:08 -0500

    changes

--- unit_test/batch/fft_indiv_test.cpp
+++ unit_test/batch/fft_indiv_test.cpp
@@ -3,47 +3,63 @@
 #include <numeric>
 #include <cmath>
 #include "c_assert.hpp"
+#include <chrono>
 
 template <int nbits>
-void test_fft(const std::array<uint32_t, 1 << nbits>& p1)
+void test_fft(const std::array<uint32_t, 1 << nbits>& p1, uint32_t num_test)
 {
-
-    std::string string_msg = "Input p1";
     constexpr int N = 1 << nbits;
-
-    print_results<uint32_t>(string_msg, p1.data(), p1.size());
+    int i;
+    auto start = std::chrono::high_resolution_clock::now();
 
     std::array<uint32_t, N> result{};
     std::fill(result.begin(), result.end(), 0);
 
     alignas(64) std::array<double, N> fft{};
-    TwistFpgaIFFT<N>(fft, p1);
+    for (i=0; i< num_test; i++) TwistFpgaIFFT<N>(fft, p1);
 
+    auto finish1 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = finish1 - start;
 
-    string_msg = "TwistIFFT 32 bit";
-    print_results<double>(string_msg,  fft.data(), fft.size());
+    for (i=0; i< num_test; i++) TwistFpgaFFT<N>(result, fft);
 
-    TwistFpgaFFT<N>(result, fft);
-    string_msg = "TwistFFT 32 bit";
-    print_results<int32_t>(string_msg,  reinterpret_cast<int32_t*>(result.data()), result.size());
+    auto finish = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> elapsed = finish - start;
+    std::chrono::duration<double, std::milli> elapsed1 = finish1 - start;
+    std::chrono::duration<double, std::milli> elapsed2 = finish - finish1;
+
+    std::cout << "Num tests: " << num_test << " s\n";
+    std::cout << "Total Elapsed IFFT time: " << elapsed1.count() << " ms\n";
+    std::cout << "Total Elapsed FFT time: " << elapsed2.count() << " ms\n";
+    std::cout << "Total Elapsed time: " << elapsed.count() << " ms\n";
 }
 
 
 
 template <class P, int nbits>
-void test_fft_p()
+void test_fft_p(uint32_t num_test)
 {
     constexpr int N = 1 << nbits;
 
     std::array<P,  N> p1{};
-    std::array<P,  N> p2{};
     std::iota(p1.begin(), p1.end(), 1);
-    test_fft<nbits>(p1);
+    test_fft<nbits>(p1, num_test);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+    uint32_t num_test = 10;
+    for(int i = 0; i < argc; i++) {
+        std::cout << "Argument " << i << " is " << argv[i] << std::endl;
+    }
+
+    if(argc > 1) {
+        std::stringstream str_stream(argv[1]);
+        str_stream >> num_test;
+    }
+
+    cout << "num iterations: " << num_test << endl;
     constexpr int nbit = 10;
-    test_fft_p<uint32_t,nbit>();
+    test_fft_p<uint32_t,nbit>(num_test);
     return 0;
 }
